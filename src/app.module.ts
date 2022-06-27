bf78cebbf92db2f134b4d10ae358eb9bed34459b
linuxlite <eze.rey92@gmail.com>
2022-06-27 14:54:28 -0300

    Car schema and forForfeature in module Cars

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,13 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
-//"mongodb://mongo/mymongodatabase"
+import { CarsModule } from './cars/cars.module';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://mongo/mymongodatabase')],
+  imports: [
+    MongooseModule.forRoot('mongodb://mongo/mymongodatabase'),
+    CarsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
