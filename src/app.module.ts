50c826cedbaec7e53ed6d70541724550e142a016
linuxlite <eze.rey92@gmail.com>
2022-06-27 14:23:12 -0300

    mongoose db forRoot

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+//"mongodb://mongo/mymongodatabase"
 
 @Module({
-  imports: [],
+  imports: [MongooseModule.forRoot('mongodb://mongo/mymongodatabase')],
   controllers: [AppController],
   providers: [AppService],
 })
