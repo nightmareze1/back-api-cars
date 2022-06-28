0a4044e9a60855e43781b6e82f366983057b4150
linuxlite <eze.rey92@gmail.com>
2022-06-28 15:07:55 -0300

    comenzando a codear user module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,13 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { CarsModule } from './cars/cars.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://mongo/mymongodatabase'),
     CarsModule,
+    UsersModule,
   ],
   controllers: [AppController],
   providers: [AppService],
---
+++ src/users/schemas/user.schema.ts
@@ -0,0 +1,22 @@
+import { Schema } from 'mongoose';
+
+export const UserSchema = new Schema({
+  username: {
+    type: String,
+    require: true,
+  },
+  email: {
+    type: String,
+    require: true,
+  },
+
+  password: {
+    type: String,
+    require: true,
+  },
+
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
