b27124d9fe021d32762e0f0bab58f081a0c8b220
linuxlite <eze.rey92@gmail.com>
2022-06-28 12:25:00 -0300

    upload a photos Cars

--- src/cars/cars.controller.ts
+++ src/cars/cars.controller.ts
@@ -14,10 +14,19 @@ import {
   HttpException,
   NotFoundException,
   Body,
+  UploadedFiles,
+  UseInterceptors,
+  HttpVersionNotSupportedException,
 } from '@nestjs/common';
+import { diskStorage } from 'multer';
+import { v4 as uuidv4 } from 'uuid';
+
+import { AnyFilesInterceptor, FileInterceptor } from '@nestjs/platform-express';
 import { CarsService } from './cars.service';
 import { CreateCarDto } from './dto/cars.dto';
 import { CarInterface } from './interfaces/cars.interface';
+import { of } from 'rxjs';
+import { join } from 'path/posix';
 
 @Controller('cars')
 export class CarsController {
@@ -60,5 +69,74 @@ export class CarsController {
   @Post('/createCar')
   createCar(@Body() createCarDto: CreateCarDto): Promise<CarInterface> {
     return this.carService.createCar(createCarDto);
+  }
+  //Upload one file
+  @Post('file')
+  @UseInterceptors(
+    FileInterceptor('file', {
+      storage: diskStorage({
+        destination: './uploads',
+        filename: (req, file, callback) => {
+          callback(null, `${uuidv4()}.jpg`);
+        },
+      }),
+      fileFilter: (req, file, callback) => {
+        const nameOriginal = file.originalname.toLocaleLowerCase();
+        // //console.log(nameOriginal);
+        if (!nameOriginal.match(/(.gif|.png|.jpg|.jpeg)$/)) {
+          return callback(
+            new HttpVersionNotSupportedException({
+              status: HttpStatus.NOT_FOUND,
+              error: `El archivo tiene una extension no valida, validas: gif png jpg jpeg`,
+            }),
+            false,
+          );
+        }
+        callback(null, true);
+      },
+    }),
+  )
+  uploadFile(@UploadedFiles() file: Express.Multer.File) {
+    return {
+      url: `Archivo ${file.filename} cargado correctamente`,
+    };
+  }
+  //Upload multiple files
+  @Post('files')
+  @UseInterceptors(
+    AnyFilesInterceptor({
+      storage: diskStorage({
+        destination: './uploads',
+        filename: (req, file, callback) => {
+          callback(null, `${uuidv4()}.jpg`);
+        },
+      }),
+      fileFilter: (req, file, callback) => {
+        const nameOriginal = file.originalname.toLocaleLowerCase();
+        if (!nameOriginal.match(/(.gif|.png|.jpg|.jpeg)$/)) {
+          return callback(
+            new HttpVersionNotSupportedException({
+              status: HttpStatus.NOT_FOUND,
+              error: `Uno de los archivos tiene una extension no valida. Validas: gif png jpg jpeg`,
+            }),
+            false,
+          );
+        }
+        callback(null, true);
+      },
+    }),
+  )
+  uploadFiles(@UploadedFiles() files: Array<Express.Multer.File>) {
+    return files.map((item) => {
+      return { url: item.filename };
+    });
+  }
+
+  @Get('uploads/:imagename')
+  findProfileImage(@Param() params, @Res() res) /* : Observable<Object>  */ {
+    console.log(params);
+    const { imagename } = params;
+
+    return of(res.sendFile(join(process.cwd(), `uploads/${imagename}`)));
   }
 }
